97232cd11ac41369cdf206677cb9d00443c27ad4
seni7 <85029836+seni7@users.noreply.github.com>
2022-06-24 16:09:10 +0300

    student interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,1 +1,40 @@
+/** Renders table based on a list of objects */
 
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const studentA: Student = {
+  firstName: 'senait',
+  lastName: 'alemayehu',
+  age: 24,
+  location: 'ethiopia',
+}
+
+const studentB: Student = {
+  firstName: 'gedion',
+  lastName: 'hailu',
+  age: 21,
+  location: 'ethiopia',
+}
+
+const studentList = [studentA, studentB];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentList.forEach((obj) => {
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
+  const cellLocation = document.createElement('td');
+  cellName.textContent = obj.firstName;
+  cellLocation.textContent = obj.location;
+  row.appendChild(cellName);
+  row.appendChild(cellLocation);
+  tbody.appendChild(row);
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
